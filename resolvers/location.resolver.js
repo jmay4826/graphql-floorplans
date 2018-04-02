64d49862ba8b9fc382809f1caede50fa705e7716
Jonathan May <jmay4826@gmail.com>
2018-04-01 23:40:41 -0400

    clean up old code

--- resolvers/location.resolver.js
+++ resolvers/location.resolver.js
@@ -16,14 +16,6 @@ const getUsers = async (rootValue, args, context, info) => {
   return users;
 };
 
-const getUsername = () => {
-  console.log("getusername in location");
-  return "jonathan";
-};
-const getRealName = () => {
-  console.log("get real name in location");
-  return "may";
-};
 const getReplies = async ({ id }, args, context) => {
   return await context.req.app.get("db").comments.find({ reply_id: id });
 };
@@ -33,7 +25,8 @@ module.exports = {
     getLocation
   },
   Location: {
-    comments: getComments
+    comments: getComments,
+    users: getUsers
   },
   Comment: {
     replies: getReplies
